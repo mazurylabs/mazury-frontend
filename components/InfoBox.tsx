3e8606bfb8ce8b4f499df4db710b6c78d60b9f92
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-03-14 17:33:22 +0530

    Fix InfoBox spacing

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -11,7 +11,7 @@ export const InfoBox: FC<InfoBoxProps> = ({ children, className }) => {
     <div
       className={`flex items-center gap-3 rounded-md bg-indigoGray-10 p-3 ${className}`}
     >
-      <div className="w-2/12">
+      <div className="w-1/12">
         <Image
           src="/icons/info.svg"
           width="20px"
