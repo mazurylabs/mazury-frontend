10d62a2ba4ac6d590b617aea353f5f16780bcc5f
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-04-01 02:02:54 +0400

    Apply sidebar audit suggestions

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -4,12 +4,15 @@ import { FC, useContext, useState } from 'react';
 import { Avatar } from '..';
 import { SidebarContext } from 'contexts';
 import { useRouter } from 'next/router';
-import Image from 'next/image';
 
 export const Sidebar: FC = () => {
   const { isOpen } = useContext(SidebarContext);
-  const { pathname } = useRouter();
-  const [searchQuery, setSearchQuery] = useState('');
+  const router = useRouter();
+  const { pathname } = router;
+
+  const goToSearch = () => {
+    router.push('/');
+  };
 
   return (
     <>
@@ -21,19 +24,14 @@ export const Sidebar: FC = () => {
         className={`${isOpen ? 'ml-4' : 'mx-auto'}`}
       />
 
-      <Divider className="mt-8" />
+      <Divider className="mx-3 mt-8" />
 
       <menu
         className={`mt-12 flex flex-col text-xl font-bold ${
           isOpen ? 'items-start' : 'items-center'
-        } h-[100%] ${isOpen && 'px-4'}`}
+        } h-[100%] px-3`}
       >
-        <SearchInput
-          value={searchQuery}
-          onChange={setSearchQuery}
-          expanded={isOpen}
-          onClick={() => {}}
-        />
+        <SearchInput expanded={isOpen} onClick={goToSearch} />
         <SidebarItem
           href="/"
           label="Home"
@@ -46,7 +44,7 @@ export const Sidebar: FC = () => {
         <div className="mt-auto flex flex-col">
           {/* Email not verified alert */}
           {isOpen && (
-            <div className="mx-auto mb-11 flex w-[168px] items-center justify-center">
+            <div className="mx-auto mb-11 flex items-center justify-center">
               <img
                 src="/icons/info.svg"
                 width="16px"
@@ -134,21 +132,17 @@ const SidebarItem: FC<SidebarItemProps> = ({
  */
 
 interface SearchInputProps {
-  value: string;
-  onChange: (value: string) => void;
   onClick: () => void;
   expanded: boolean;
 }
 
-const SearchInput: FC<SearchInputProps> = ({
-  value,
-  onChange,
-  onClick,
-  expanded,
-}) => {
+const SearchInput: FC<SearchInputProps> = ({ onClick, expanded }) => {
   return (
     <div
-      className={`flex items-center justify-center gap-2 rounded-xl bg-indigoGray-90 p-4 text-[14px] font-normal text-indigo-50 shadow-md`}
+      role="button"
+      className={`${
+        expanded ? 'justify-left' : 'justify-center'
+      } flex w-full items-center gap-2 rounded-xl bg-indigoGray-90 p-3 text-[14px] font-normal text-indigo-50 shadow-md hover:cursor-pointer`}
       onClick={onClick}
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -158,12 +152,9 @@ const SearchInput: FC<SearchInputProps> = ({
         className={`h-[16px] w-[16px] ${!expanded && 'hover:cursor-pointer'}`}
       />
       {expanded && (
-        <input
-          className="h-[16px] w-3/4 bg-transparent text-sm font-semibold outline-none"
-          placeholder="Search"
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-        />
+        <span className="flex h-[16px] w-3/4 items-center bg-transparent text-sm font-semibold outline-none">
+          Search
+        </span>
       )}
     </div>
   );
@@ -180,5 +171,5 @@ interface DividerProps {
 }
 
 const Divider: FC<DividerProps> = ({ className }) => {
-  return <hr className={`w-full border border-indigoGray-10 ${className}`} />;
+  return <hr className={`border border-indigoGray-20 ${className}`} />;
 };
