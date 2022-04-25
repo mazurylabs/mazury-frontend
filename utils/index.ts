eec235a4f96e9a08129925752a1b8a446a9e82af
Chris <christianotu.d@gmail.com>
2022-04-25 06:34:21 +0100

    Added function to separate numbers with commas

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Referral, Skill } from 'types';
+import { Profile, Referral, Skill } from 'types';
 import { theme } from '../tailwind.config';
 
 export const getTruncatedAddress = (
@@ -113,4 +113,26 @@ export const getMetricDisplayValue = (
   placeholder: string = '-'
 ) => {
   return value === null || value === undefined ? placeholder : value;
+};
+
+export const commify = (value: number) => {
+  let numberString: string | string[] = String(value);
+
+  numberString = numberString.split('').reverse();
+
+  numberString = numberString.reduce((prev, next, index) => {
+    let shouldComma = (index + 1) % 3 === 0 && index + 1 < numberString.length;
+
+    let updatedValue = `${prev}${next}`;
+
+    if (shouldComma) {
+      updatedValue = `${updatedValue},`;
+    }
+
+    return updatedValue;
+  }, '');
+
+  numberString = numberString.split('').reverse().join('');
+
+  return numberString;
 };
