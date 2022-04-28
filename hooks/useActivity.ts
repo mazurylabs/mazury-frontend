38f3064661317fa409e207cdd4b3414faa0c046f
Chris <christianotu.d@gmail.com>
2022-04-28 20:02:18 +0100

    Updated with params for query limit and endpoint

--- hooks/useActivity.ts
+++ hooks/useActivity.ts
@@ -1,10 +1,17 @@
 import useSWR from 'swr';
 import { ListResponse, Activity } from '../types';
 
-export const useActivity = (address: string) => {
+export const useActivity = (
+  address: string,
+  options?: { isNetwork?: boolean; limit?: number }
+) => {
   // TODO: Pagination
+
+  const networkParam = options?.isNetwork ? 'network' : 'user';
+  const limitParam = options?.limit ? '&limit=' + options?.limit : '';
+
   const { data, error } = useSWR<ListResponse<Activity>>(
-    `/activity?user=${address}`
+    `/activity?${networkParam}=${address}${limitParam}`
   );
 
   return {
