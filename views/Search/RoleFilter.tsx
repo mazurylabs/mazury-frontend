9512e9ba8f7ef0b45fe3655e17110e56329af958
Chris <christianotu.d@gmail.com>
2022-05-30 00:31:37 +0100

    Updated with logic for role filtering

--- views/Search/RoleFilter.tsx
+++ views/Search/RoleFilter.tsx
@@ -1,11 +1,84 @@
+import { motion } from 'framer-motion';
+import SVG from 'react-inlinesvg';
+
+import { useScreenWidth } from 'hooks';
+import { FilterType } from 'types';
+import { fadeAnimation, trayAnimation } from 'utils';
+
 interface RoleFilterProps {
   handleSelect: (filter: string) => void;
+  handleGoBack: (filter: FilterType) => void;
 }
 
-export const RoleFilter = ({}: RoleFilterProps) => {
+const roles = [
+  { title: 'developer', icon: 'developer', supply: 658 },
+  { title: 'designer', icon: 'designer', supply: 658 },
+  { title: 'trader', icon: 'trader', supply: 658 },
+  { title: 'creator', icon: 'creator', supply: 658 },
+  { title: 'researcher', icon: 'researcher', supply: 658 },
+  { title: 'investor', icon: 'investor', supply: 658 },
+  { title: 'community manager', icon: 'community', supply: 658 },
+];
+
+export const RoleFilter = ({ handleGoBack, handleSelect }: RoleFilterProps) => {
+  const screenWidth = useScreenWidth();
+
+  const variants =
+    screenWidth > 768 ? {} : screenWidth <= 640 ? trayAnimation : fadeAnimation;
+
   return (
-    <div className="h-full w-full">
-      <p>Hello Role Filter</p>
-    </div>
+    <motion.div
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className="flex h-[434px] w-full !cursor-default flex-col rounded-3xl bg-white p-6 shadow-base md:h-[434px] md:w-[500px] lg:h-[390px]"
+    >
+      <div className="mb-6 lg:hidden">
+        <button
+          type="button"
+          className="flex space-x-4"
+          onClick={() => handleGoBack('empty')}
+        >
+          <div className="h-6 w-6">
+            <SVG src="/icons/back.svg" height={24} width={24} />
+          </div>
+          <span className="font-sans text-xl font-medium leading-[30px] text-indigoGray-90">
+            Badges
+          </span>
+        </button>
+      </div>
+
+      <div>
+        <ul className="grid grid-cols-3 gap-3">
+          {roles.map((role) => (
+            <li
+              key={role.title}
+              className="flex h-[100px] max-h-[100px] cursor-pointer flex-col items-center justify-center space-y-2 rounded-lg border"
+              onClick={() =>
+                handleSelect('role_' + role.title.split(' ').join('_'))
+              }
+            >
+              <div className="h-6 w-6 space-y-2">
+                <SVG
+                  src={`/icons/roles/${role.icon}.svg`}
+                  height={24}
+                  width={24}
+                />
+              </div>
+
+              <div className="text-wrap space-y-2 font-sans">
+                <p className="indigoGray-50 text-center text-xs font-bold uppercase leading-[18px]">
+                  <span>{role.title}</span> <br />
+                  <span className="font-normal text-indigoGray-40 ">
+                    ({role.supply})
+                  </span>
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </motion.div>
   );
 };
