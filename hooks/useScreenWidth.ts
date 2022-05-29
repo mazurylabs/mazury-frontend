2f54dd67a9e82680a4e543383929fc56bdcaf90c
Chris <christianotu.d@gmail.com>
2022-05-30 00:18:52 +0100

    Updated to get screen size initial render

--- hooks/useScreenWidth.ts
+++ hooks/useScreenWidth.ts
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
 
 export const useScreenWidth = () => {
-  const [screenWidth, setScreenWidth] = useState<number>();
+  const [screenWidth, setScreenWidth] = useState<number>(() => {
+    if (typeof window !== 'undefined') {
+      return window?.innerWidth;
+    } else {
+      return 0;
+    }
+  });
 
   useEffect(() => {
     const handleResize = () => setScreenWidth(window.innerWidth);
