438891913824e52b574477128bf72ab2909e70b5
Chris <christianotu.d@gmail.com>
2022-05-31 16:11:46 +0100

    Updated with data for empty state

--- views/Search/EmptyState.tsx
+++ views/Search/EmptyState.tsx
@@ -1,7 +1,49 @@
+import SVG from 'react-inlinesvg';
+import { useAccount } from 'wagmi';
+
+import { useProfileSuggestions } from 'hooks';
+
+import { Talent } from './Talent';
+
 export const EmptyState = () => {
+  const [{ data: accountData }] = useAccount();
+  const { profiles } = useProfileSuggestions(accountData?.address, {
+    isNetwork: true,
+    limit: 3,
+  });
+
   return (
-    <div className="font-inter mt-3 text-sm font-medium text-indigoGray-90">
-      <p>No suggestions found</p>
+    <div className="font-inter mt-3 flex grow flex-col pb-6 text-sm font-medium text-indigoGray-90">
+      <div className="flex grow flex-col items-center justify-between pt-[80px] pb-8">
+        <div className="space-y-[2px] text-center">
+          <h3 className="indigoGray-80 font-sans text-lg font-bold leading-[27px]">
+            Let’s try again
+          </h3>
+          <p>
+            We found no perfect matches, you can try <br /> with a different
+            keyword
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center space-y-[10px]">
+          <p className="font-inter text-base font-bold leading-6 text-pink-600">
+            or explore our spotlight!
+          </p>
+
+          <SVG src="/icons/spotlight-arrow.svg" height={58} width={85} />
+        </div>
+      </div>
+
+      <div className="mt-auto space-y-4">
+        <div>
+          <h4 className="font-serif text-xl font-bold leading-6 text-pink-600">
+            Mazury Spotlight
+          </h4>
+        </div>
+        <div>
+          <Talent result={profiles} />
+        </div>
+      </div>
     </div>
   );
 };
