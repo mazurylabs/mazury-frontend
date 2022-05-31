05243542f1909e284d0e3dac5bd8b1b5b35a9024
Chris <christianotu.d@gmail.com>
2022-05-31 21:12:19 +0100

    Implemented pagination

--- views/Search/SearchResults.tsx
+++ views/Search/SearchResults.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import ScrollLock from 'react-scrolllock';
-import clsx from 'clsx';
 
 import { Profile } from 'types';
 import { Talent } from './Talent';
@@ -9,25 +8,6 @@ interface Props {
   result: Profile[];
 }
 
-const Tag = ({
-  label,
-  highlighted,
-}: {
-  label?: string;
-  highlighted?: boolean;
-}) => {
-  return (
-    <p
-      className={clsx(
-        highlighted && 'bg-emerald-50 text-emerald-900',
-        'flex h-fit w-fit items-center rounded bg-indigoGray-5 px-2 py-1 font-sans text-xs font-bold text-indigoGray-90'
-      )}
-    >
-      {(label as string)[0].toUpperCase() + label?.slice(1)}
-    </p>
-  );
-};
-
 export const SearchResults = React.forwardRef<HTMLDivElement, Props>(
   ({ result }, ref) => {
     return (
@@ -35,6 +15,9 @@ export const SearchResults = React.forwardRef<HTMLDivElement, Props>(
         <div className="my-6 h-[100px] grow overflow-y-auto">
           <div className="m-1 h-fit space-y-4">
             <Talent result={result} />
+            {result.length >= 20 && (
+              <div ref={ref} className="h-[1px] w-full bg-transparent" />
+            )}
           </div>
         </div>
       </ScrollLock>
