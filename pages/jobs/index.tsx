3ca0e395f419ea71ed5c6c458ede27a756b4af3f
Chris <christianotu.d@gmail.com>
2022-08-30 08:32:46 +0100

    created initial jobs page

---
+++ pages/jobs/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from 'components';
+
+const Jobs = () => {
+  return (
+    <Layout variant="plain">
+      <div>
+        <h1>This is the jobs page</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Jobs;
