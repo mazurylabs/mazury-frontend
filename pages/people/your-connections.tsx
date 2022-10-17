12f41d7101f01514d56a45a73e75137976413d33
ayoMoses <odukoyaayodeji@gmail.com>
2022-10-17 15:37:56 +0100

    update conections ui

--- pages/people/your-connections.tsx
+++ pages/people/your-connections.tsx
@@ -1,7 +1,23 @@
+import { Layout } from '@/components';
+import Link from 'next/link';
+import SVG from 'react-inlinesvg';
 import React from 'react';
 
 const YourConnections = () => {
-  return <div>YourConnections</div>;
+  return (
+    <Layout variant="plain">
+      <div className="mt-9 px-4 lg:mt-16 lg:px-8">
+        <div className="mb-3 w-full border lg:divide-y lg:border-indigoGray-20 lg:pb-4">
+          <Link href="/">
+            <a className="h-6 w-6">
+              <SVG src="/icons/arrow-left.svg" height={24} width={24} />
+              <span className="sr-only">Go back</span>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default YourConnections;
