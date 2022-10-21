6c1e8abd943d948c8d8d19c92cf6280857f3a173
Ayodeji Moses Odukoya <35398374+AyoMoses@users.noreply.github.com>
2022-10-21 14:16:19 +0100

    update ui fixes for modal, sticky, and show connections to recruiters (#198)
    * update ui fixes for modal, sticky, and show connections to recruiters
    
    * Fixes
    
    * Remove sticky
    
    Co-authored-by: wojtekwtf <wojciechkulikowski@icloud.com>

--- pages/people/connections.tsx
+++ pages/people/connections.tsx
@@ -1,7 +1,6 @@
 import { Layout } from '@/components';
 import Link from 'next/link';
 import SVG from 'react-inlinesvg';
-import Image from 'next/image';
 import React from 'react';
 import { useConnections } from '../../hooks';
 
@@ -16,8 +15,8 @@ const Connections = () => {
 
   return (
     <Layout variant="plain">
-      <div className="mt-9 mb-10 px-4 lg:mt-16 lg:px-8">
-        <div>
+      <div className="mb-10 px-4 lg:mt-16 lg:px-8">
+        <div className="w-full bg-white pt-9">
           <Link href="/">
             <a className="h-6 w-6">
               <SVG src="/icons/arrow-left.svg" height={24} width={24} />
@@ -47,25 +46,29 @@ const Connections = () => {
         <div className="mt-[34px] grid w-full grid-cols-1 gap-y-7 lg:mt-0 lg:grid-cols-2 lg:gap-6">
           {connections?.map((connection, index) => (
             <div key={index}>
-              <div className="flex items-center">
-                <img
-                  className="h-10 w-10 overflow-hidden rounded-full"
-                  src={connection.requested_profile.avatar}
-                  alt={connection.requested_profile.username}
-                />
-                <div className="ml-3 flex flex-col">
-                  <h4 className="font-serif text-base lowercase text-indigoGray-70">
-                    {connection.requested_profile.username}
-                  </h4>
-                  <p
-                    className={`font-sans text-xs font-medium capitalize ${
-                      connectionStatusColor[connection.status]
-                    }`}
-                  >
-                    {connection.status}
-                  </p>
-                </div>
-              </div>
+              <Link
+                href={`/people/${connection.requested_profile.eth_address}`}
+              >
+                <a className="flex items-center">
+                  <img
+                    className="h-10 w-10 overflow-hidden rounded-full"
+                    src={connection.requested_profile.avatar}
+                    alt={connection.requested_profile.username}
+                  />
+                  <div className="ml-3 flex flex-col">
+                    <h4 className="font-serif text-base lowercase text-indigoGray-70">
+                      {connection.requested_profile.username}
+                    </h4>
+                    <p
+                      className={`font-sans text-xs font-medium capitalize ${
+                        connectionStatusColor[connection.status]
+                      }`}
+                    >
+                      {connection.status}
+                    </p>
+                  </div>
+                </a>
+              </Link>
             </div>
           ))}
         </div>
