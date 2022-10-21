d17b9333143b583989b6fea16614c4f123612645
wojtekwtf <wojciechkulikowski@icloud.com>
2022-10-21 15:01:08 +0100

    Change recommended profiles

--- views/Search/EmptyState.tsx
+++ views/Search/EmptyState.tsx
@@ -1,18 +1,328 @@
+import { axios } from '@/lib/axios';
+import { useEffect, useState } from 'react';
 import SVG from 'react-inlinesvg';
 
-import { useProfileSuggestions } from 'hooks';
-
 import { Talent } from './Talent';
-import { useSelector } from 'react-redux';
-import { userSlice } from '@/selectors';
+
+const TEMPORARY_RECOMMENDED_DATA = [
+  {
+    id: '5ac19001-cf6d-42c1-af62-37e7dc96137f',
+    top_badges: [
+      {
+        badge_type: {
+          id: 'da0cec2b-9618-4b9d-8587-120fad50ff92',
+          issuer: {
+            name: 'poap',
+          },
+          image:
+            'https://assets.poap.xyz/i-met-justinlerouxeth-at-devcon-6-2022-logo-1664209215930.png',
+          poap_badge_extra_data: {
+            id: '17e1ed46-1d1b-4a7f-8efc-0fa775fc88ee',
+            created_at: '2022-09-27T15:31:00.889604Z',
+            updated_at: '2022-10-15T22:31:45.265411Z',
+            url: 'https://devcon.org/',
+            start_date: '2022-10-11',
+            end_date: '2022-10-14',
+            expired: true,
+            expiry_date: '2022-10-14',
+            last_synced: '2022-10-15',
+            event_id: 67360,
+            image_url:
+              'https://assets.poap.xyz/i-met-justinlerouxeth-at-devcon-6-2022-logo-1664209215930.png',
+          },
+          created_at: '2022-09-27T15:31:00.906749Z',
+          updated_at: '2022-10-21T00:01:55.469858Z',
+          video: null,
+          title: 'I met justinleroux.eth at Devcon 6',
+          slug: 'i-met-justinlerouxeth-at-devcon-6-2022',
+          description:
+            'This POAP proves that the bearer met justinleroux.eth at Devcon 6 and scanned their ENS card.',
+          external_image_url: null,
+          total_supply: 17,
+        },
+      },
+      {
+        badge_type: {
+          id: '6cea073e-c0e7-47a2-b22a-efbf280bddc2',
+          issuer: {
+            name: 'poap',
+          },
+          image:
+            'https://assets.poap.xyz/i-met-hudsoneth-at-devcon-6-2022-logo-1665459689606.png',
+          poap_badge_extra_data: {
+            id: '6933309d-796b-4535-bf53-f50619da7a3a',
+            created_at: '2022-10-11T15:30:45.186931Z',
+            updated_at: '2022-10-15T19:15:42.176601Z',
+            url: 'https://devcon.org/',
+            start_date: '2022-10-11',
+            end_date: '2022-10-14',
+            expired: true,
+            expiry_date: '2022-10-14',
+            last_synced: '2022-10-15',
+            event_id: 74422,
+            image_url:
+              'https://assets.poap.xyz/i-met-hudsoneth-at-devcon-6-2022-logo-1665459689606.png',
+          },
+          created_at: '2022-10-11T15:30:45.215844Z',
+          updated_at: '2022-10-21T00:01:22.358289Z',
+          video: null,
+          title: 'I met hudson.eth at Devcon 6',
+          slug: 'i-met-hudsoneth-at-devcon-6-2022',
+          description:
+            'This POAP proves that the bearer met hudson.eth at Devcon 6 and scanned their ENS card.',
+          external_image_url: null,
+          total_supply: 59,
+        },
+      },
+      {
+        badge_type: {
+          id: '427cf78f-341f-48fe-903e-af04b00be374',
+          issuer: {
+            name: 'poap',
+          },
+          image:
+            'https://assets.poap.xyz/tomosaito-chiva-lounge-2022-logo-1665701042572.png',
+          poap_badge_extra_data: {
+            id: 'ecb146b7-6d70-4129-b82b-9e08faeb28cf',
+            created_at: '2022-10-14T15:30:43.458952Z',
+            updated_at: '2022-10-20T17:23:28.598730Z',
+            url: '',
+            start_date: '2022-10-13',
+            end_date: '2022-10-13',
+            expired: false,
+            expiry_date: '2022-11-13',
+            last_synced: '2022-10-20',
+            event_id: 76257,
+            image_url:
+              'https://assets.poap.xyz/tomosaito-chiva-lounge-2022-logo-1665701042572.png',
+          },
+          created_at: '2022-10-14T15:30:43.464796Z',
+          updated_at: '2022-10-21T00:01:49.888174Z',
+          video: null,
+          title: 'Tomosaito - Chiva Lounge',
+          slug: 'tomosaito-chiva-lounge-2022',
+          description:
+            'Tomo is a multi talented mixer, drummer, and designer for the Ethereum Foundation.\r\n\r\nDevcon 6, Day 3 - 10.13.2022',
+          external_image_url: null,
+          total_supply: 32,
+        },
+      },
+    ],
+    avatar:
+      'https://mazury-staging.s3.eu-central-1.amazonaws.com/gradient5.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUV5WL77G5SO2Y7UV%2F20221021%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20221021T135538Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=4576ac96605f985e696353ffd2953bf7027ce7ad4f48f6ad22a5e2d65f0c95ae',
+    eth_address: '0xB6d052d6F5921d52C1c14b69a02De04f840CeFCd',
+    bio: '',
+    username: 'wackerow.eth',
+    ens_name: 'wackerow.eth',
+    github: '',
+    twitter: '',
+    website: '',
+    credentials_count: 386,
+  },
+  {
+    id: '9b0ffee8-ce9a-4191-855c-ae69ba7d0304',
+    top_badges: [
+      {
+        badge_type: {
+          id: '762b12aa-6aa7-4530-b1e7-636e07c1a75c',
+          issuer: {
+            name: 'poap',
+          },
+          image:
+            'https://assets.poap.xyz/so-col-x-lootbox-fund-twitter-space-f09f91bef09f8e99-2022-logo-1666007943220.png',
+          poap_badge_extra_data: {
+            id: '8d7ca6cf-0e4e-4980-98ed-9f1969ffe48b',
+            created_at: '2022-10-17T15:30:58.794884Z',
+            updated_at: '2022-10-20T18:25:35.439393Z',
+            url: 'https://discord.gg/socol',
+            start_date: '2022-10-18',
+            end_date: '2022-10-18',
+            expired: false,
+            expiry_date: '2022-11-18',
+            last_synced: '2022-10-20',
+            event_id: 76876,
+            image_url:
+              'https://assets.poap.xyz/so-col-x-lootbox-fund-twitter-space-f09f91bef09f8e99-2022-logo-1666007943220.png',
+          },
+          created_at: '2022-10-17T15:30:58.814605Z',
+          updated_at: '2022-10-21T00:01:24.964539Z',
+          video: null,
+          title: 'SO-COL x Lootbox Fund Twitter Space 👾🎙',
+          slug: 'so-col-x-lootbox-fund-twitter-space-f09f91bef09f8e99-2022',
+          description: 'SO-COL x Lootbox Fund Twitter Space TOMORROW 👾🎙',
+          external_image_url: null,
+          total_supply: 59,
+        },
+      },
+      {
+        badge_type: {
+          id: '614edd32-930e-4b8a-a869-70c02c324637',
+          issuer: {
+            name: 'poap',
+          },
+          image:
+            'https://assets.poap.xyz/ultra-sound-poap-event-2022-logo-1663527819992.gif',
+          poap_badge_extra_data: {
+            id: '25094cc6-d9e1-4254-bae2-5676921b4487',
+            created_at: '2022-09-19T15:30:49.986776Z',
+            updated_at: '2022-10-20T17:59:18.813190Z',
+            url: 'https://ultrasound.money',
+            start_date: '2022-09-15',
+            end_date: '2022-09-15',
+            expired: false,
+            expiry_date: '2023-09-15',
+            last_synced: '2022-10-20',
+            event_id: 65873,
+            image_url:
+              'https://assets.poap.xyz/ultra-sound-poap-event-2022-logo-1663527819992.gif',
+          },
+          created_at: '2022-09-19T15:30:50.001728Z',
+          updated_at: '2022-10-21T00:04:25.147007Z',
+          video: null,
+          title: 'ultra sound POAP',
+          slug: 'ultra-sound-poap-event-2022',
+          description:
+            'celebrating the ultra sound fam 🦇🔊👪\r\n\r\na special POAP for 1,559 pre-merge believers\r\n\r\nmeet the meme spreaders on ultrasound.money',
+          external_image_url: null,
+          total_supply: 473,
+        },
+      },
+      {
+        badge_type: {
+          id: '1d9ce68f-cf47-49a9-916e-61cb7b3ecd27',
+          issuer: {
+            name: 'sismo',
+          },
+          image:
+            'https://sismo-prod-hub-static.s3.eu-west-1.amazonaws.com/badges/proof-of-attendance-main-events.svg',
+          poap_badge_extra_data: null,
+          created_at: '2022-10-04T15:13:44.600439Z',
+          updated_at: '2022-10-21T00:02:06.127627Z',
+          video: null,
+          title: 'Proof of Attendance ZK Badge',
+          slug: 'proof-of-attendance-zk-badge',
+          description:
+            'ZK Badge owned by Ethereum events attendees. This Badge proves their IRL attendance to at least one Ethereum event.',
+          external_image_url:
+            'https://sismo-prod-hub-static.s3.eu-west-1.amazonaws.com/badges/proof-of-attendance-main-events.svg',
+          total_supply: 60,
+        },
+      },
+    ],
+    avatar:
+      'https://mazury-staging.s3.eu-central-1.amazonaws.com/gradient8.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUV5WL77G5SO2Y7UV%2F20221021%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20221021T135538Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=eb69afd83c6b18f02de042788fd8afefa4cc94987b49c021145b4cb99cf7cd62',
+    eth_address: '0x8135d8CCd2AcBE575Ebf1827349185df7185Cf97',
+    bio: '',
+    username: 'remi.eth',
+    ens_name: 'remi.eth',
+    github: '',
+    twitter: '',
+    website: '',
+    credentials_count: 64,
+  },
+  {
+    id: 'faea5b99-0ea9-44fe-8f9b-902405b9270a',
+    top_badges: [
+      {
+        badge_type: {
+          id: 'fc12e581-8236-4d6c-8fb2-d1c7b3882722',
+          issuer: {
+            name: 'buildspace',
+          },
+          image:
+            'https://tokens.buildspace.so/assets/CHb51f7d0f-8bf0-4fb2-bc4c-c8cfcfaf67ff-51/render.png',
+          poap_badge_extra_data: null,
+          created_at: '2022-09-08T10:00:30.335979Z',
+          updated_at: '2022-10-21T00:01:47.133901Z',
+          video: null,
+          title: 'Intro to Web3',
+          slug: 'buildspace_intro_web3',
+          description:
+            "A 2-week project where you'll learn some Solidity, write + deploy a smart contract to the blockchain, and build a Web3 client app to interact with your contract. Perfect for hackers curious about crypto.",
+          external_image_url:
+            'https://tokens.buildspace.so/assets/CHb51f7d0f-8bf0-4fb2-bc4c-c8cfcfaf67ff-51/render.png',
+          total_supply: 6543,
+        },
+      },
+      {
+        badge_type: {
+          id: '0925596e-e60f-437a-8719-6b0b1f80b0b8',
+          issuer: {
+            name: 'poap',
+          },
+          image:
+            'https://assets.poap.xyz/paradigm-ctf-2022-2022-logo-1661818929100.png',
+          poap_badge_extra_data: {
+            id: '49125dbe-3114-4a0c-9600-ee2485609790',
+            created_at: '2022-08-30T15:30:27.382726Z',
+            updated_at: '2022-09-20T16:13:26.521062Z',
+            url: 'https://ctf.paradigm.xyz',
+            start_date: '2022-08-20',
+            end_date: '2022-08-22',
+            expired: true,
+            expiry_date: '2022-09-19',
+            last_synced: '2022-09-20',
+            event_id: 62691,
+            image_url:
+              'https://assets.poap.xyz/paradigm-ctf-2022-2022-logo-1661818929100.png',
+          },
+          created_at: '2022-08-30T15:30:27.391423Z',
+          updated_at: '2022-10-21T00:01:32.894096Z',
+          video: null,
+          title: 'Paradigm CTF 2022',
+          slug: 'paradigm-ctf-2022-2022',
+          description:
+            'This POAP was given to those who solved at least one challenge in Paradigm CTF 2022',
+          external_image_url: null,
+          total_supply: 438,
+        },
+      },
+      {
+        badge_type: {
+          id: '57840265-9d1f-4073-9c39-3dcdd671d6df',
+          issuer: {
+            name: 'poap',
+          },
+          image: null,
+          poap_badge_extra_data: {
+            id: '20a330bd-ff4f-49a1-8759-28b759396c1f',
+            created_at: '2022-04-26T09:18:08.957361Z',
+            updated_at: '2022-08-26T23:13:05.246477Z',
+            url: 'https://www.guildofguardians.com/',
+            start_date: '2021-06-02',
+            end_date: '2021-06-09',
+            expired: true,
+            expiry_date: '2021-07-09',
+            last_synced: '2022-08-26',
+            event_id: 2598,
+            image_url: null,
+          },
+          created_at: '2022-04-26T09:18:08.968945Z',
+          updated_at: '2022-10-21T00:01:12.867246Z',
+          video: null,
+          title: 'Guild of Guardians Founder Badge',
+          slug: 'guild-of-guardians-founder-badge-2021',
+          description:
+            'A special, exclusive Founders Badge for early members of the Guardian community. ',
+          external_image_url: null,
+          total_supply: 27903,
+        },
+      },
+    ],
+    avatar:
+      'https://mazury-staging.s3.eu-central-1.amazonaws.com/gradient8.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUV5WL77G5SO2Y7UV%2F20221021%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20221021T135538Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=eb69afd83c6b18f02de042788fd8afefa4cc94987b49c021145b4cb99cf7cd62',
+    eth_address: '0xE913c0f42fB60F85Bd43bDdDAf4e004135eF318c',
+    bio: '',
+    username: '0xE913c0f42fB60F85Bd43bDdDAf4e004135eF318c',
+    ens_name: '',
+    github: '',
+    twitter: '',
+    website: '',
+    credentials_count: 19,
+  },
+];
 
 export const EmptyState = () => {
-  const { address } = useSelector(userSlice);
-  const { profiles } = useProfileSuggestions(address as string, {
-    isNetwork: true,
-    limit: 3,
-  });
-
   return (
     <div className="mt-3 flex grow flex-col pb-6 font-sans text-sm font-medium text-indigoGray-90">
       <div className="flex grow flex-col items-center justify-between pt-[80px] pb-8">
@@ -28,7 +338,7 @@ export const EmptyState = () => {
 
         <div className="flex flex-col items-center space-y-[10px]">
           <p className="font-sans text-base font-bold leading-6 text-pink-600">
-            or explore our spotlight!
+            or explore our recommended profiles!
           </p>
 
           <SVG src="/icons/spotlight-arrow.svg" height={58} width={85} />
@@ -38,11 +348,11 @@ export const EmptyState = () => {
       <div className="mt-auto space-y-4">
         <div>
           <h4 className="font-serif text-xl font-bold leading-6 text-pink-600">
-            Mazury Spotlight
+            Recommended profiles
           </h4>
         </div>
         <div>
-          <Talent result={profiles} />
+          <Talent result={TEMPORARY_RECOMMENDED_DATA} />
         </div>
       </div>
     </div>
