152b0e429acf11f8fb3a4e0d7bd1e1002d9b95f1
Chris <christian@mazury.xyz>
2023-02-06 20:50:21 +0100

    add require auth

--- views/Profile/Container.tsx
+++ views/Profile/Container.tsx
@@ -1,14 +1,13 @@
 import { useRouter } from 'next/router';
 import * as React from 'react';
 import SVG from 'react-inlinesvg';
-import { useQuery } from 'react-query';
+import { AnimatePresence } from 'framer-motion';
 
 import { Button, Modal } from 'components';
 import { Navbar } from './Navbar';
 import { NavItem } from './type';
-import { axios } from 'lib/axios';
-import { Badge, CredentialsCount } from 'types';
 import { useCredentialCount } from 'hooks';
+import { RequireSignin } from '@/components/RequireSignin';
 
 interface ContainerProps {
   summary: React.ReactElement;
@@ -47,70 +46,76 @@ export const Container: React.FC<ContainerProps> & {
   };
 
   return (
-    <div className="flex grow space-x-6 pb-10">
-      {summary}
+    <>
+      <div className="flex grow space-x-6 pb-10">
+        {summary}
 
-      <div className="relative flex grow flex-col">
-        <div className="sticky top-0 z-20 bg-white pt-10 pb-6">
-          {title ? (
-            <div className="flex justify-between">
-              <div className="flex space-x-2">
-                <button aria-label="back" onClick={handleBack}>
-                  <SVG height={24} width={24} src="/icons/chevron-left.svg" />
-                </button>
-                <p className="font-sansMid text-2xl font-medium text-indigoGray-90">
-                  {title}
-                </p>
+        <div className="relative flex grow flex-col">
+          <div className="sticky top-0 z-20 bg-white pt-10 pb-6">
+            {title ? (
+              <div className="flex justify-between">
+                <div className="flex space-x-2">
+                  <button aria-label="back" onClick={handleBack}>
+                    <SVG height={24} width={24} src="/icons/chevron-left.svg" />
+                  </button>
+                  <p className="font-sansMid text-2xl font-medium text-indigoGray-90">
+                    {title}
+                  </p>
+                </div>
+
+                {handleSave && (
+                  <Button
+                    className="w-[200px]"
+                    onClick={handleSave}
+                    loading={!toggleSaveModal && isSaving}
+                  >
+                    Save
+                  </Button>
+                )}
               </div>
-
-              {handleSave && (
-                <Button
-                  className="w-[200px]"
-                  onClick={handleSave}
-                  loading={!toggleSaveModal && isSaving}
-                >
-                  Save
-                </Button>
-              )}
-            </div>
-          ) : (
-            <Navbar links={navItems || []} />
-          )}
-        </div>
-        <div className="max-w-[826.6px] grow overflow-scroll">{children}</div>
-        <Modal
-          isOpen={toggleSaveModal}
-          onClose={() => setToggleSaveModal(false)}
-        >
-          <div className="w-fit max-w-[300px]">
-            <h3 className="font-demi text-3xl text-indigoGray-90">
-              Are you sure?
-            </h3>
-            <p className="mt-2 font-sansMid text-sm font-medium text-indigoGray-60">
-              You have unsaved changes that will be lost if you exit now
-            </p>
-
-            <div className="mt-4 flex w-fit justify-between space-x-4">
-              <Button
-                className="w-[140px] shrink-0 !border !border-indigoGray-20 !bg-indigoGray-10 !text-indigoGray-90 !shadow-base"
-                variant="secondary"
-                onClick={handleDontSave}
-              >
-                Don't save
-              </Button>
-              <Button
-                className="w-[140px] shrink-0 !px-0 !font-semibold"
-                variant="primary"
-                onClick={handleSave}
-                loading={isSaving}
-              >
-                Save changes
-              </Button>
-            </div>
+            ) : (
+              <Navbar links={navItems || []} />
+            )}
           </div>
-        </Modal>
+          <div className="max-w-[826.6px] grow overflow-scroll">{children}</div>
+          <Modal
+            isOpen={toggleSaveModal}
+            onClose={() => setToggleSaveModal(false)}
+          >
+            <div className="w-fit max-w-[300px]">
+              <h3 className="font-demi text-3xl text-indigoGray-90">
+                Are you sure?
+              </h3>
+              <p className="mt-2 font-sansMid text-sm font-medium text-indigoGray-60">
+                You have unsaved changes that will be lost if you exit now
+              </p>
+
+              <div className="mt-4 flex w-fit justify-between space-x-4">
+                <Button
+                  className="w-[140px] shrink-0 !border !border-indigoGray-20 !bg-indigoGray-10 !text-indigoGray-90 !shadow-base"
+                  variant="secondary"
+                  onClick={handleDontSave}
+                >
+                  Don't save
+                </Button>
+                <Button
+                  className="w-[140px] shrink-0 !px-0 !font-semibold"
+                  variant="primary"
+                  onClick={handleSave}
+                  loading={isSaving}
+                >
+                  Save changes
+                </Button>
+              </div>
+            </div>
+          </Modal>
+        </div>
       </div>
-    </div>
+
+      <AnimatePresence>
+        <RequireSignin />
+      </AnimatePresence>
+    </>
   );
 };
 
