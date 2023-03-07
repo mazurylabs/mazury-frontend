3febac22fb21444ffe6d7e4ed7045a3ca448039b
Chris <christian@mazury.xyz>
2023-03-07 17:27:40 +0100

    route to credential

--- views/Profile/Overview/Idle.tsx
+++ views/Profile/Overview/Idle.tsx
@@ -289,7 +289,9 @@ const CredentialsSection: React.FC<{
                   key={id}
                   title={badge_type.title}
                   description={badge_type.description}
-                  onSelect={() => {}}
+                  onSelect={() =>
+                    router.push(`${router.asPath}/credentials/${id}`)
+                  }
                   imageSrc={badge_type.image}
                   totalSupply={badge_type.total_supply}
                 />
