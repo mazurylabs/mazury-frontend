a42419785cb034aec55c76e7a0878eeae2e86e1b
Chris <christian@mazury.xyz>
2023-03-15 10:46:45 +0100

    remove unused code

--- hooks/useBadgeTypes.ts
+++ hooks/useBadgeTypes.ts
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import { BadgeType, ListResponse } from '../types';
 
@@ -10,28 +9,6 @@ export const useBadgeTypes = (issuer: string = 'mazury') => {
   return {
     badgeTypes: data?.results,
     nextBadgeType: data?.next || '',
-    error,
-  };
-};
-
-export const useTotalBadgeCounts = () => {
-  const { badgeTypes, error } = useBadgeTypes();
-  const [totalBadgeCounts, setTotalBadgeCounts] = useState<{
-    [key: string]: number;
-  }>({});
-
-  useEffect(() => {
-    if (badgeTypes) {
-      const badgeCounts: { [key: string]: number } = {};
-      badgeTypes.forEach((badgeType) => {
-        badgeCounts[badgeType.id] = badgeType.total_supply!;
-      });
-      setTotalBadgeCounts(badgeCounts);
-    }
-  }, [badgeTypes]);
-
-  return {
-    totalBadgeCounts,
     error,
   };
 };
