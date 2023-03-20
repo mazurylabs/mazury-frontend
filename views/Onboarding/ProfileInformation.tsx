a482492c15f56411110034cd1895327d7678190c
wojtekwtf <wojciechkulikowski@icloud.com>
2023-03-20 22:40:06 +0000

    Add profile type and terms of service

--- views/Onboarding/ProfileInformation.tsx
+++ views/Onboarding/ProfileInformation.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import SVG from 'react-inlinesvg';
 
-import { Button, Input } from '@/components';
+import Link from 'next/link';
+import { Button, Checkbox, Input } from '@/components';
 import { useOnboardingContext } from '@/providers/onboarding/OnboardingProvider';
 import { OnboardingStepsEnum } from '@/providers/onboarding/types';
 import { isValid } from '@/utils/api';
@@ -12,6 +13,8 @@ export const ProfileInformation = () => {
   const [loading, setLoading] = React.useState(false);
   const [usernameError, setUsernameError] = React.useState<string>('');
   const [emailError, setEmailError] = React.useState<string>('');
+  const [isRecruiter, setIsRecruiter] = React.useState<boolean>(false);
+  const [tosAccepted, setTosAccepted] = React.useState<boolean>(false);
 
   const { data: userProfile } = useUser();
   const { handleStep, handleSetProfile, profile } = useOnboardingContext();
@@ -39,6 +42,7 @@ export const ProfileInformation = () => {
     setLoading(false);
 
     if (!emailError && !usernameError) {
+      // TODO upload all data?
       handleStep(OnboardingStepsEnum['ALLSET']);
     } else if (emailError && usernameError) {
       setEmailError('Email already exists');
@@ -54,7 +58,7 @@ export const ProfileInformation = () => {
     <>
       <div className="mb-[90px] space-y-8 sm:mb-[128px] sm:space-y-6 xl:mb-[32px]">
         <div className="space-y-3">
-          <h2 className="font-demi text-4xl text-indigoGray-90">Hi there</h2>
+          <h2 className="font-demi text-4xl text-indigoGray-90">Hi there!</h2>
           <p className="font-sansSemi text-sm font-medium text-indigoGray-60">
             We’re glad you’re joining us. Let’s take a moment to polish your
             profile a bit!
@@ -80,13 +84,13 @@ export const ProfileInformation = () => {
               label={
                 <div
                   className={`font-sans text-${
-                    emailError ? 'red-500' : 'indigoGray-40'
+                    emailError ? 'red-500' : 'indigoGray-40 mb-0.5'
                   }`}
                 >
                   Username{' '}
                   <span
                     className={`font-sans font-normal text-${
-                      emailError ? 'red-500' : 'indigoGray-40'
+                      emailError ? 'red-500' : 'indigoGray-30'
                     }`}
                   >
                     (Required)
@@ -117,13 +121,13 @@ export const ProfileInformation = () => {
               label={
                 <div
                   className={`font-sans text-${
-                    emailError ? 'red-500' : 'indigoGray-40'
+                    emailError ? 'red-500' : 'indigoGray-40 mb-0.5'
                   }`}
                 >
                   E-mail{' '}
                   <span
                     className={`font-sans font-normal text-${
-                      emailError ? 'red-500' : 'indigoGray-40'
+                      emailError ? 'red-500' : 'indigoGray-30'
                     }`}
                   >
                     (Required)
@@ -153,47 +157,91 @@ export const ProfileInformation = () => {
           </div>
 
           <div>
-            <div className="font-sans text-sm text-indigoGray-40">
+            <div className="mb-0.5 font-sans text-sm text-indigoGray-40">
               Profile type{' '}
               <span className="font-sans font-normal text-indigoGray-30">
                 (Required)
               </span>
             </div>
-            <div className="space-y-4">
+            <div className="flex flex-row space-x-1 md:space-x-4">
               <button
-                className="flex w-full justify-between rounded-lg border border-indigoGray-20 p-6"
-                // onClick={() => handleNext(true)}
+                type="button"
+                className={`flex w-full justify-between rounded-lg py-3 px-4 ${
+                  !isRecruiter
+                    ? `border-[1.5px] border-indigo-600 bg-indigo-50`
+                    : `border border-indigoGray-20`
+                }`}
+                onClick={() => {
+                  handleSetProfile('is_recruiter', false);
+                  setIsRecruiter(false);
+                }}
               >
-                <span className="font-sansMid text-base font-medium text-indigoGray-90">
-                  I’m a recruiter
+                <span className="font-sansMid text-sm font-medium text-indigoGray-90">
+                  I’m a professional
                 </span>
-                <SVG src="/icons/arrow-right.svg" height={24} width={24} />
               </button>
 
               <button
-                className="flex w-full justify-between rounded-lg border border-indigoGray-20 p-6"
-                // onClick={() => handleNext(false)}
+                type="button"
+                className={`flex w-full justify-between rounded-lg py-3 px-4 ${
+                  isRecruiter
+                    ? `border-[1.5px] border-indigo-600 bg-indigo-50`
+                    : `border border-indigoGray-20`
+                }`}
+                onClick={() => {
+                  handleSetProfile('is_recruiter', true);
+                  setIsRecruiter(false);
+                }}
               >
-                <span className="font-sansMid text-base font-medium text-indigoGray-90">
-                  I’m a professional
+                <span className="font-sansMid text-sm font-medium text-indigoGray-90">
+                  I’m a recruiter
                 </span>
-                <SVG src="/icons/arrow-right.svg" height={24} width={24} />
               </button>
             </div>
           </div>
         </div>
       </div>
 
-      <Button
-        type="submit"
-        size="large"
-        className="!mb-12 w-full xl:mb-0"
-        disabled={!profile.username || !profile.email}
-        loading={loading}
-        onClick={handleSubmit}
-      >
-        Continue
-      </Button>
+      <div>
+        <div className="mb-2 flex flex-row items-center space-x-2">
+          <Checkbox
+            innerClassName="h-4 w-4"
+            outerClassName="h-4 w-4"
+            checked={tosAccepted}
+            setChecked={() => setTosAccepted(!tosAccepted)}
+            label=""
+            id={'acceptTos'}
+          />
+          <p className="text-sm">
+            By checking you agree to the{' '}
+            <Link
+              className="font-medium text-indigo-600"
+              href={'/privacy-policy'}
+              target="_blank"
+            >
+              privacy-policy
+            </Link>{' '}
+            and the{' '}
+            <Link
+              className="font-medium text-indigo-600"
+              href={'/terms-of-service'}
+              target="_blank"
+            >
+              terms of service
+            </Link>
+          </p>
+        </div>
+        <Button
+          type="submit"
+          size="large"
+          className="!mb-12 w-full xl:mb-0"
+          disabled={!profile.username || !profile.email || !tosAccepted}
+          loading={loading}
+          onClick={handleSubmit}
+        >
+          Continue
+        </Button>
+      </div>
     </>
   );
 };
