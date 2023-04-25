c3c11361aa3fb6d1f148221f7366a38852aa2563
Chris <christian@mazury.xyz>
2023-04-25 16:35:18 +0100

    add loading screen, alignment updates, prefill applicant form

--- pages/opportunities/[opportunityId]/index.tsx
+++ pages/opportunities/[opportunityId]/index.tsx
@@ -13,9 +13,8 @@ import { CustomInput } from 'views/Opportunities/CustomInput';
 import { useRouter } from 'next/router';
 import { useAlert } from 'components/Alert.tsx';
 import clsx from 'clsx';
-import { EditStepsEnum } from './edit';
 import { axios } from '@/lib/axios';
-import { CompanyType, ListResponse, OpportunityType } from '@/types';
+import { CompanyType, ListResponse, OpportunityType, Profile } from '@/types';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 interface Props {
@@ -34,17 +33,8 @@ interface Application {
 }
 
 const Opportunity: React.FC<Props> = ({ opportunityId }) => {
-  const router = useRouter();
-
-  const { data } = useUser();
-  const isMobile = useMobile(false);
-  const { dispatch } = useAlert({});
-
   const { data: opportunity, isLoading } = useOpportunity({ opportunityId });
-
-  if (!opportunity) {
-    return <h1>Loading...</h1>;
-  }
+  const { data } = useUser();
 
   return (
     <Layout
@@ -54,135 +44,16 @@ const Opportunity: React.FC<Props> = ({ opportunityId }) => {
     >
       <div className="flex flex-col w-full lg:items-center pb-10 pt-6 lg:px-0 xl:pt-16">
         <div className="lg:w-[730px] xl:w-[1000px] space-y-6 lg:space-y-4">
-          <div className="flex items-center justify-between">
-            <div className="space-y-3">
-              <div className="flex space-x-3 items-center">
-                <Link href={`/`} passHref>
-                  <SVG src="/icons/chevron-left.svg" width={24} height={24} />
-                  <span className="sr-only">back</span>
-                </Link>
-                <p className="font-sans font-medium text-2xl text-indigoGray-90">
-                  {capitalize(
-                    isMobile
-                      ? truncateString(opportunity.title, 15)
-                      : opportunity.title
-                  )}
-                </p>
-              </div>
-              <div className="flex space-x-6">
-                <div className="flex items-center space-x-3">
-                  <Avatar
-                    src={opportunity.company_info.logo}
-                    alt="company"
-                    outerClassName="h-8 w-8"
-                    className="border-[0.6px] border-indigoGray-30"
-                  />
-                  <p className="font-sans text-sm font-medium text-indigoGray-90">
-                    {capitalize(opportunity.company_info.name)}
-                  </p>
-                </div>
-                <p className="font-sans font-medium text-sm text-indigoGray-40 flex items-center">
-                  <SVG src="/icons/time.svg" className="mr-1" />
-                  {dayjs(opportunity.created_at).fromNow()}
-                </p>
-              </div>
-            </div>
-
-            {data?.is_recruiter ? (
-              <RecruiterView
-                handleViewApplicants={() =>
-                  router.push(`/opportunities/${opportunityId}/applicants`)
-                }
-                handleEdit={() =>
-                  router.push(`/opportunities/${opportunityId}/edit`)
-                }
-                handleUnpublish={() =>
-                  dispatch({ type: 'notification', message: 'Unpublished' })
-                }
-              />
-            ) : (
-              <ApplicantView opportunityId={opportunityId} />
-            )}
-          </div>
-
-          {data?.is_recruiter && (
-            <Button
-              size="medium"
-              variant="secondary"
-              className="lg:hidden w-full bg-transparent"
-              onClick={() =>
-                router.push(`/opportunities/${opportunityId}/applicants`)
-              }
-            >
-              {/* See 3 candidates */}
-              See candidates
-              <SVG src="/icons/chevron-right.svg" className="h-4 w-4 ml-2" />
-            </Button>
+          {isLoading && !opportunity ? (
+            <LoadingView isRecruiter={!!data?.is_recruiter} />
+          ) : (
+            <OpportunityView
+              opportunity={opportunity as OpportunityWithCompany}
+              opportunityId={opportunityId}
+              isRecruiter={!!data?.is_recruiter}
+              user={data as Profile}
+            />
           )}
-
-          <div className="xl:w-[700px] space-y-4 lg:space-y-6 font-sans text-sm font-medium text-indigoGray-90">
-            <div className="space-y-4 p-4 border border border-indigoGray-20 rounded-lg">
-              <div className="flex flex-col justify-between space-y-4 lg:flex-row lg:space-y-0">
-                <div className="space-y-2">
-                  <p className="text-indigoGray-40">Company name</p>
-                  <div className="flex space-x-3">
-                    <p>{opportunity.company_info.name}</p>
-                    {data?.is_recruiter && (
-                      <Link
-                        href={`/opportunities/${opportunityId}/edit?company-id=${opportunity.company_info.id}`}
-                        className=" text-indigo-600"
-                      >
-                        Edit company information
-                      </Link>
-                    )}
-                  </div>
-                </div>
-                <div className="space-y-2">
-                  <p className="text-indigoGray-40">Link</p>
-                  <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href={opportunity.website}
-                    className="text-indigoGray-90 flex"
-                  >
-                    {truncateString(opportunity.website, 40)}
-                  </a>
-                </div>
-              </div>
-
-              <div className="flex flex-col justify-between lg:flex-row lg:space-y-0">
-                <div className="space-y-2">
-                  <p className="text-indigoGray-40">Location</p>
-                  <p>
-                    {opportunity.location} | {opportunity.work_mode}
-                  </p>
-                </div>
-                <div className="space-y-2">
-                  <p className="text-indigoGray-40">Company size</p>
-                  <p>{opportunity.company_info.size} people work here</p>
-                </div>
-              </div>
-
-              <div className="space-y-2">
-                <p className="text-indigoGray-40">Compensation range</p>
-                <p>{opportunity.salary}</p>
-              </div>
-
-              <div className="space-y-2">
-                <p className="text-indigoGray-40">Company description</p>
-                <p className="text-justify">
-                  {opportunity.company_info.description}
-                </p>
-              </div>
-            </div>
-
-            <div className="space-y-2">
-              <p className="text-indigoGray-40">Description</p>
-              <p className="text-justify">
-                {capitalize(opportunity.description)}
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </Layout>
@@ -191,10 +62,230 @@ const Opportunity: React.FC<Props> = ({ opportunityId }) => {
 
 export default Opportunity;
 
+const LoadingView = ({ isRecruiter }: { isRecruiter: boolean }) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="space-y-3">
+          <div className="h-6 w-[273px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          <div className="flex items-center space-x-4">
+            <div className="h-8 w-8 animate-pulse rounded-full bg-indigoGray-30" />
+            <div className="h-3 w-[147px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          </div>
+        </div>
+
+        <div>
+          {isRecruiter ? (
+            <RecruiterView />
+          ) : (
+            <ApplicantView opportunityId={''} />
+          )}
+        </div>
+      </div>
+
+      <div className="xl:w-[700px] space-y-4 lg:space-y-6 font-sans text-sm font-medium text-indigoGray-90">
+        <div className="flex-col space-y-8 lg:space-y-0 p-4 border border border-indigoGray-20 rounded-lg flex flex lg:flex-row">
+          <div className="grow space-y-8 lg:space-y-4">
+            <div className="space-y-2">
+              <div className="h-3 w-[212px] animate-pulse rounded-2xl bg-indigoGray-30" />
+              <div className="h-3 w-[147px] animate-pulse rounded-2xl bg-indigoGray-30" />
+            </div>
+            <div className="space-y-2">
+              <div className="h-3 w-[212px] animate-pulse rounded-2xl bg-indigoGray-30" />
+              <div className="h-3 w-[147px] animate-pulse rounded-2xl bg-indigoGray-30" />
+            </div>
+            <div className="space-y-2">
+              <div className="h-3 w-[212px] animate-pulse rounded-2xl bg-indigoGray-30" />
+              <div className="h-3 w-[147px] animate-pulse rounded-2xl bg-indigoGray-30" />
+            </div>
+          </div>
+
+          <div className="grow space-y-8 lg:space-y-4">
+            <div className="space-y-2">
+              <div className="h-3 w-[212px] animate-pulse rounded-2xl bg-indigoGray-30" />
+              <div className="h-3 w-[147px] animate-pulse rounded-2xl bg-indigoGray-30" />
+            </div>
+            <div className="space-y-2">
+              <div className="h-3 w-[212px] animate-pulse rounded-2xl bg-indigoGray-30" />
+              <div className="h-3 w-[147px] animate-pulse rounded-2xl bg-indigoGray-30" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="space-y-2">
+        <p className="font-sans font-medium text-sm text-indigoGray-40">
+          Description
+        </p>
+        <div className="space-y-3">
+          <div className="h-3 w-[90%] lg:w-[370px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          <div className="h-3 w-[85%] lg:w-[390px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          <div className="h-3 w-[90%] lg:w-[370px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          <div className="h-3 w-[85%] lg:w-[390px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          <div className="h-3 w-[90%] lg:w-[370px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          <div className="h-3 w-[85%] lg:w-[390px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          <div className="h-3 w-[90%] lg:w-[370px] animate-pulse rounded-2xl bg-indigoGray-30" />
+          <div className="h-3 w-[85%] lg:w-[390px] animate-pulse rounded-2xl bg-indigoGray-30" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const OpportunityView = ({
+  opportunity,
+  opportunityId,
+  isRecruiter,
+  user,
+}: {
+  opportunity: OpportunityWithCompany;
+  opportunityId: string;
+  isRecruiter: boolean;
+  user: Profile;
+}) => {
+  const router = useRouter();
+
+  const isMobile = useMobile(false);
+  const { dispatch } = useAlert({});
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="space-y-3">
+          <div className="flex space-x-3 items-center">
+            <Link href={`/`} passHref>
+              <SVG src="/icons/chevron-left.svg" width={24} height={24} />
+              <span className="sr-only">back</span>
+            </Link>
+            <p className="font-sans font-medium text-2xl text-indigoGray-90">
+              {capitalize(
+                isMobile
+                  ? truncateString(opportunity.title, 15)
+                  : opportunity.title
+              )}
+            </p>
+          </div>
+          <div className="flex space-x-6">
+            <div className="flex items-center space-x-3">
+              <Avatar
+                src={opportunity.company_info.logo}
+                alt="company"
+                outerClassName="h-8 w-8"
+                className="border-[0.6px] border-indigoGray-30"
+              />
+              <p className="font-sans text-sm font-medium text-indigoGray-90">
+                {capitalize(opportunity.company_info.name)}
+              </p>
+            </div>
+            <p className="font-sans font-medium text-sm text-indigoGray-40 flex items-center">
+              <SVG src="/icons/time.svg" className="mr-1" />
+              {dayjs(opportunity.created_at).fromNow()}
+            </p>
+          </div>
+        </div>
+
+        {isRecruiter ? (
+          <RecruiterView
+            handleViewApplicants={() =>
+              router.push(`/opportunities/${opportunityId}/applicants`)
+            }
+            handleEdit={() =>
+              router.push(`/opportunities/${opportunityId}/edit`)
+            }
+            handleUnpublish={() =>
+              dispatch({ type: 'notification', message: 'Unpublished' })
+            }
+          />
+        ) : (
+          <ApplicantView opportunityId={opportunityId} user={user} />
+        )}
+      </div>
+
+      {isRecruiter && (
+        <Button
+          size="medium"
+          variant="secondary"
+          className="lg:hidden w-full bg-transparent"
+          onClick={() =>
+            router.push(`/opportunities/${opportunityId}/applicants`)
+          }
+        >
+          {/* See 3 candidates */}
+          See candidates
+          <SVG src="/icons/chevron-right.svg" className="h-4 w-4 ml-2" />
+        </Button>
+      )}
+
+      <div className="xl:w-[700px] space-y-4 lg:space-y-6 font-sans text-sm font-medium text-indigoGray-90">
+        <div className="space-y-4 p-4 border border border-indigoGray-20 rounded-lg">
+          <div className="flex flex-col justify-between space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
+            <div className="space-y-2 lg:w-[calc(50%-8px)]">
+              <p className="text-indigoGray-40">Company name</p>
+              <div className="flex space-x-3">
+                <p>{opportunity.company_info.name}</p>
+                {isRecruiter && (
+                  <Link
+                    href={`/opportunities/${opportunityId}/edit?company-id=${opportunity.company_info.id}`}
+                    className=" text-indigo-600"
+                  >
+                    Edit company information
+                  </Link>
+                )}
+              </div>
+            </div>
+
+            <div className="space-y-2 lg:w-[calc(50%-8px)]">
+              <p className="text-indigoGray-40">Link</p>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={opportunity.website}
+                className="text-indigoGray-90 flex shrink-0"
+              >
+                {truncateString(opportunity.website, 40)}
+              </a>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-between space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
+            <div className="space-y-2 lg:w-[calc(50%-8px)]">
+              <p className="text-indigoGray-40">Location</p>
+              <p>
+                {opportunity.location} | {opportunity.work_mode}
+              </p>
+            </div>
+            <div className="space-y-2 lg:w-[calc(50%-8px)]">
+              <p className="text-indigoGray-40">Company size</p>
+              <p>{opportunity.company_info.size} people work here</p>
+            </div>
+          </div>
+
+          <div className="space-y-2">
+            <p className="text-indigoGray-40">Compensation range</p>
+            <p>{opportunity.salary}</p>
+          </div>
+
+          <div className="space-y-2">
+            <p className="text-indigoGray-40">Company description</p>
+            <p className="text-justify">
+              {opportunity.company_info.description}
+            </p>
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <p className="text-indigoGray-40">Description</p>
+          <p className="text-justify">{capitalize(opportunity.description)}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
 interface RecruiterViewProps {
-  handleEdit: () => void;
-  handleUnpublish: () => void;
-  handleViewApplicants: () => void;
+  handleEdit?: () => void;
+  handleUnpublish?: () => void;
+  handleViewApplicants?: () => void;
 }
 
 const RecruiterView: React.FC<RecruiterViewProps> = ({
@@ -207,15 +298,18 @@ const RecruiterView: React.FC<RecruiterViewProps> = ({
   return (
     <Popover.Root>
       <div className="flex items-center space-x-4">
-        <Popover.Trigger asChild className="relative">
-          <button type="button" aria-label="comments">
-            <SVG src="/icons/more.svg" className="mr-1" />
-          </button>
-        </Popover.Trigger>
+        {(handleEdit || handleUnpublish) && (
+          <Popover.Trigger asChild className="relative">
+            <button type="button" aria-label="comments">
+              <SVG src="/icons/more.svg" className="mr-1" />
+            </button>
+          </Popover.Trigger>
+        )}
         <Button
           onClick={handleViewApplicants}
           size="medium"
           className="hidden h-[37px] lg:flex"
+          disabled={!handleViewApplicants}
         >
           {/* <SVG src="/icons/user.svg" className="mr-2 h-4 w-4" />0 applications */}
           <SVG src="/icons/user.svg" className="mr-2 h-4 w-4" />
@@ -252,8 +346,9 @@ const RecruiterView: React.FC<RecruiterViewProps> = ({
   );
 };
 
-const ApplicantView: React.FC<{ opportunityId: string }> = ({
+const ApplicantView: React.FC<{ opportunityId: string; user?: Profile }> = ({
   opportunityId,
+  user,
 }) => {
   const [application, setApplication] = React.useState<Application>();
   const isMobile = useMobile();
@@ -271,7 +366,15 @@ const ApplicantView: React.FC<{ opportunityId: string }> = ({
   };
 
   const { mutate, isLoading, isSuccess } = useMutation({
-    mutationFn: () => apply({ opportunityId, application }),
+    mutationFn: () => {
+      const payload = {
+        ...application,
+        email: application?.email || user?.email || '',
+        website: application?.website || user?.website || '',
+      };
+
+      return apply({ opportunityId, application: payload as Application });
+    },
   });
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -282,7 +385,10 @@ const ApplicantView: React.FC<{ opportunityId: string }> = ({
   return (
     <Popover.Root>
       <Popover.Trigger className="fixed left-0 bottom-0 py-2 lg:relative">
-        <Button className="mx-4 lg:mx-0 mb-4 lg:mb-0 w-[calc(100vw-32px)] lg:w-[211px]">
+        <Button
+          disabled={!opportunityId}
+          className="mx-4 lg:mx-0 mb-4 lg:mb-0 w-[calc(100vw-32px)] lg:w-[211px]"
+        >
           Apply
         </Button>
       </Popover.Trigger>
@@ -342,7 +448,7 @@ const ApplicantView: React.FC<{ opportunityId: string }> = ({
                     <CustomInput
                       label="Email"
                       placeholder="Insert email"
-                      value={application?.email}
+                      value={application?.email || user?.email}
                       onChange={(value) => handleChange({ email: value })}
                     />
 
@@ -375,7 +481,7 @@ const ApplicantView: React.FC<{ opportunityId: string }> = ({
                       placeholder="Insert website"
                       info="Own website, Twitter, discord or any other"
                       required={false}
-                      value={application?.website}
+                      value={application?.website || user?.website}
                       onChange={(value) => handleChange({ website: value })}
                     />
 
